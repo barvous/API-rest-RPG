bdece9a3ed18e41856b239a47acd99ae0fd2a046
Marcos Daniel <marcos.coelho@wmtrading.com.br>
2022-03-17 16:23:14 -0300

    Criando o projeto spring boot

---
+++ server/src/test/java/com/marcos/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
